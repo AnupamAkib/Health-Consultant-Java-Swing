b64fb15b93c4fae4c5c34ba71de98ab781ddd69f
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2020-04-09 20:39:08 +0600

    Add files via upload

---
+++ src/healthconsultant/HealthConsultant.java
@@ -0,0 +1,11 @@
+
+package healthconsultant;
+
+public class HealthConsultant {
+    public static void main(String args[]){
+        test ob = new test();
+        ob.setVisible(true);
+        ob.setResizable(false);
+    }
+}
+
---
+++ src/healthconsultant/Main.java
@@ -0,0 +1,13 @@
+package healthconsultant;
+
+/*
+package healthcalculator;
+
+public class Main {
+    public static void main(String args[]){
+        test ob = new test();
+        ob.setVisible(true);
+        ob.setResizable(false);
+    }
+}
+*/
